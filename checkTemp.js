e935f52e1cdd9b7e566866e2cb127f371c6bb8a3
Antonm <55620364+notnaone@users.noreply.github.com>
2022-10-11 10:23:06 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,7 @@
+var checkTemp = prompt('Mis on temperatuur?')
+if (checkTemp >= 0 && checkTemp <= 20)
+    console.log('-1')
+else if (checkTemp >= 21 && checkTemp <= 40)
+    console.log('0')
+else if (checkTemp >=41 && checkTemp <= 60)
+    console.log('1')
